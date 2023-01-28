eb9006e84c66418ad05a960ceda3d917b6507afe
Protocol Corporation <protocollive@outlook.com>
2023-01-28 09:47:34 -0300

    Docs and webhooks methods

--- src/Trello.php
+++ src/Trello.php
@@ -1,13 +1,16 @@
 <?php
 //Protocol Corporation Ltda.
 //https://github.com/ProtocolLive/Trello
-//2023.01.27.03
+//2023.01.28.00
 
 namespace ProtocolLive\Trello;
+use CurlHandle;
+use Exception;
 use stdClass;
 
 final class Trello{
   private const Url = 'https://api.trello.com/1/';
+  private CurlHandle $Curl;
 
   public function __construct(
     private string $Key,
@@ -20,6 +23,11 @@ final class Trello{
     return json_decode($return);
   }
   
+  /**
+   * @link https://developer.atlassian.com/cloud/trello/rest/api-group-boards/#api-boards-id-cards-get
+   * @link https://developer.atlassian.com/cloud/trello/rest/api-group-lists/#api-lists-id-cards-get
+   * @link https://developer.atlassian.com/cloud/trello/rest/api-group-members/#api-members-id-cards-get
+   */
   public function CardsGet(
     string $List = null,
     string $Board = null,
@@ -35,6 +43,9 @@ final class Trello{
     return json_decode($return);
   }
 
+  /**
+   * @link https://developer.atlassian.com/cloud/trello/rest/api-group-cards/#api-cards-id-get
+   */
   public function CardGet(
     string $Card
   ):stdClass{
@@ -42,22 +53,42 @@ final class Trello{
     return json_decode($return);
   }
 
+  /**
+   * @throws Exception
+   */
   private function Curl(
     string $Url,
-    array $Post = null
+    array $Get = [],
+    array $Post = null,
+    string $Type = null
   ):string|bool{
-    $Url = self::Url . $Url . '?key=' . $this->Key . '&token=' . $this->Token;
-    $curl = curl_init($Url);
-    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($curl, CURLOPT_USERAGENT, 'Protocol Trello library');
-    curl_setopt($curl, CURLOPT_CAINFO, __DIR__ . '/cacert.pem');
+    $Get['key'] = $this->Key;
+    $Get['token'] = $this->Token;
+    $Url = self::Url . $Url . '?' . http_build_query($Get);
+    $this->Curl = curl_init($Url);
+    curl_setopt($this->Curl, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($this->Curl, CURLOPT_USERAGENT, 'Protocol Trello library');
+    curl_setopt($this->Curl, CURLOPT_CAINFO, __DIR__ . '/cacert.pem');
     if($Post !== null):
-      curl_setopt($curl, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
-      curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($Post));
+      curl_setopt($this->Curl, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
+      curl_setopt($this->Curl, CURLOPT_POSTFIELDS, json_encode($Post));
     endif;
-    return curl_exec($curl);
+    if($Type !== null):
+      curl_setopt($this->Curl, CURLOPT_CUSTOMREQUEST, $Type);
+    endif;
+    $this->Log('Send: ' . $Url . PHP_EOL . json_encode($Post, JSON_PRETTY_PRINT));
+    $return = curl_exec($this->Curl);
+    $this->Log('Response:' . PHP_EOL . json_encode($return, JSON_PRETTY_PRINT));
+    if(curl_getinfo($this->Curl, CURLINFO_HTTP_CODE) === 200):
+      return $return;
+    else:
+      throw new Exception($return);
+    endif;
   }
 
+  /**
+   * https://developer.atlassian.com/cloud/trello/rest/api-group-boards/#api-boards-id-lists-get
+   */
   public function ListsGet(
     string $Board
   ):array{
@@ -69,12 +100,23 @@ final class Trello{
     string $Msg
   ):void{
     file_put_contents(
-      $this->DirLogs . '/send.log',
+      $this->DirLogs . '/trello.log',
       $Msg . PHP_EOL,
       FILE_APPEND
     );
   }
 
+  /**
+   * @link https://developer.atlassian.com/cloud/trello/guides/rest-api/authorization/#authorizing-a-client
+   */
+  public function MeGet():stdClass{
+    $return = $this->Curl('members/me');
+    return json_decode($return);
+  }
+
+  /**
+   * @link https://developer.atlassian.com/cloud/trello/rest/api-group-members/#api-members-id-get
+   */
   public function MemberGet(
     string $Id
   ):stdClass{
@@ -82,10 +124,56 @@ final class Trello{
     return json_decode($return);
   }
 
+  /**
+   * @link https://developer.atlassian.com/cloud/trello/rest/api-group-boards/#api-boards-id-members-get
+   */
   public function MembersGet(
     string $Board
   ):array{
     $return = $this->Curl('boards/' . $Board . '/members');
+    return json_decode($return);
+  }
+
+  /**
+   * @link https://developer.atlassian.com/cloud/trello/rest/api-group-webhooks/#api-webhooks-id-delete
+   */
+  public function WebhookDel(
+    string $Id
+  ):void{
+    $this->Curl('webhooks/' . $Id, Type: 'DELETE');
+  }
+
+  /**
+   * @link https://developer.atlassian.com/cloud/trello/rest/api-group-webhooks/#api-webhooks-id-get
+   */
+  public function WebhookGet(
+    string $Id = null
+  ):array|stdClass{
+    if($Id !== null):
+      $return = $this->Curl('webhooks/' . $Id);
+      return json_decode($return);
+    endif;
+    $return = $this->Curl('members/me/tokens', ['webhooks' => true]);
+    $return = json_decode($return);
+    return $return[0]->webhooks;
+  }
+
+  /**
+   * @link https://developer.atlassian.com/cloud/trello/rest/api-group-webhooks/#api-webhooks-post
+   */
+  public function WebhookSet(
+    string $Description,
+    string $Url,
+    string $Model
+  ):stdClass{
+    $return = $this->Curl(
+      'tokens/' . $this->Token . '/webhooks',
+      Post: [
+        'description' => $Description,
+        'callbackURL' => $Url,
+        'idModel' => $Model,
+      ]
+    );
     return json_decode($return);
   }
 }
