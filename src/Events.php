a18f8cb5ea08e511acf5b73a46ac70c1375897f7
Protocol Corporation <protocollive@outlook.com>
2023-01-28 15:48:08 -0300

    Changed events texts and new events

--- src/Events.php
+++ src/Events.php
@@ -1,15 +1,21 @@
 <?php
 //Protocol Corporation Ltda.
 //https://github.com/ProtocolLive/Trello
-//2023.01.28.00
+//2023.01.28.01
 
 namespace ProtocolLive\Trello;
 
 /**
- * Webhook events
+ * Webhook events - translationKey, because its more detailed
  */
 enum Events:string{
-  case CardMemberAdd = 'addMemberToCard';
-  case CardMemberDel = 'removeMemberFromCard';
-  case CardUpdated = 'updateCard';
+  case CardArchived = 'action_archived_card';
+  case CardDescription = 'action_changed_description_of_card';
+  case CardAdd = 'action_create_card';
+  case CardDel = 'action_delete_card';
+  case CardLabelAdd = 'action_add_label_to_card';
+  case CardMemberAdd = 'action_added_member_to_card';
+  case CardMemberDel = 'action_removed_member_from_card';
+  case CardMoved = 'action_move_card_from_list_to_list';
+  case CardRenamed = 'action_renamed_card';
 }
